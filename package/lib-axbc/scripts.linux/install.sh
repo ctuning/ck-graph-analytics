94b94aaf26f33cca56a358c5798a08e9e0dd4b39
Flavio Vella <flavio@dividiti.com>
2018-04-04 11:40:03 +0100

    Add AxBC package

---
+++ package/lib-axbc/scripts.linux/install.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+#
+# Installation script for Caffe.
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# - Flavio Vella, 2018.
+#
+
+# PACKAGE_DIR
+# INSTALL_DIR
+
+echo "**************************************************************"
+echo "Preparing vars for BC ..."
+
+
+
+CK_OPENMP="-fopenmp"
+if [ "${CK_HAS_OPENMP}" = "0"  ]; then
+  CK_OPENMP=""
+fi
+
+echo ${INSTALL_DIR}
+DEST_DIR=${INSTALL_DIR}/install/bin/
+
+if [ ! -d ${DEST_DIR} ] ; then
+      mkdir -p ${DEST_DIR}
+  fi
+
+cd ${INSTALL_DIR}/src/src
+      make \
+           DEST_DIR=${DEST_DIR}  \
+           GRAPH_GEN_LIB=$CK_ENV_LIB_GRAPH_GENERATOR_LIB \
+           MPI_HOME=${CK_ENV_LIB_MPI} \
+           CUDA_HOME=${CUDA_HOME} \
+           CUDA_ARCH=${CUDA_ARC} 
+
+
+if [ "${?}" != "0" ] ; then
+  echo "Error: make failed!"
+  exit 1
+fi
+
+
+return 0
