c7a28a61dbbc2caee14ef490284e31e4a76a9743
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-04-12 15:26:19 +0100

    a few fixes (netcdf) and seissol

--- package/lib-seissol/scripts.linux/install.sh
+++ package/lib-seissol/scripts.linux/install.sh
@@ -13,6 +13,12 @@ cd ${INSTALL_DIR}/src/
 PREFIX=${INSTALL_DIR}/install
 mkdir -p $PREFIX
 
+COMPILER_TYPE="gcc"
+if [ "${CK_CC}" == "icc" ] ; then
+   COMPILER_TYPE="intel"
+fi
+
+
 #FIX ME USE CK VARIABLES
 # i.e.,  arch: .  Valid values are: ['snoarch', 'dnoarch', 'swsm', 'dwsm', 'ssnb', 'dsnb', 'sknc', 'dknc', 'shsw', 'dhsw', 'sknl', 'dknl'
 echo $CK_ENV_LIB_HDF5
@@ -21,6 +27,7 @@ scons -j 4  \
        arch=dhsw \
        parallelization=hybrid commThread=yes \
        netcdf=yes netcdfDir=${CK_ENV_LIB_NETCDF} \
+       compiler=${COMPILER_TYPE} \
        hdf5=yes hdf5Dir=${CK_ENV_LIB_HDF5}
 
 if [ "${?}" != "0" ] ; then
