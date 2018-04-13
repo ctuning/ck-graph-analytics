610fead653fc86a0e63c1311bed2ad5a422e3ecc
Flavio Vella <flavio@dividiti.com>
2018-04-13 10:25:39 +0100

    Add first soft entry for SeisSol. To be fixed

---
+++ package/lib-seissol/scripts.linux/post-install.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+#
+# Installation script for Caffe.
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# - Grigori Fursin, 2017
+#
+# PACKAGE_DIR
+# INSTALL_DIR
+
+echo "Post install script"
+
+#DEST_SRC=`ck find program:demo-projectq`
+#echo ${INSTALL_DIR}
+#cp ${INSTALL_DIR}/src/examples/*.py ${DEST_SRC}
+DEST_SRC=${INSTALL_DIR}/install/launch_SeisSol
+mkdir -p ${DEST_SRC}
+
+cp ${INSTALL_DIR}/build/SeisSol* launch_SeisSol/
+
+
+
+return 0
