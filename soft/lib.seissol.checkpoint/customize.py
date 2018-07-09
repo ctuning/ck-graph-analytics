8eae21002833e37fc66e0033f2ae08d79012cb25
Grigori Fursin <Grigori.Fursin@cTuning.org>
2018-07-09 17:20:13 +0100

    added soft:lib.seissol.checkpoint

---
+++ soft/lib.seissol.checkpoint/customize.py
@@ -0,0 +1,90 @@
+#
+# Collective Knowledge (individual environment - setup)
+#
+# See CK LICENSE.txt for licensing details
+# See CK COPYRIGHT.txt for copyright details
+#
+# Developer: Grigori Fursin, Grigori.Fursin@cTuning.org, http://fursin.net
+#
+
+import os
+
+##############################################################################
+# setup environment setup
+
+def setup(i):
+    """
+    Input:  {
+              cfg              - meta of this soft entry
+              self_cfg         - meta of module soft
+              ck_kernel        - import CK kernel module (to reuse functions)
+
+              host_os_uoa      - host OS UOA
+              host_os_uid      - host OS UID
+              host_os_dict     - host OS meta
+
+              target_os_uoa    - target OS UOA
+              target_os_uid    - target OS UID
+              target_os_dict   - target OS meta
+
+              target_device_id - target device ID (if via ADB)
+
+              tags             - list of tags used to search this entry
+
+              env              - updated environment vars from meta
+              customize        - updated customize vars from meta
+
+              deps             - resolved dependencies for this soft
+
+              interactive      - if 'yes', can ask questions, otherwise quiet
+            }
+
+    Output: {
+              return       - return code =  0, if successful
+                                         >  0, if error
+              (error)      - error text if return > 0
+
+              bat          - prepared string for bat file
+            }
+
+    """
+
+    # Get variables
+    ck=i['ck_kernel']
+    s=''
+
+    iv=i.get('interactive','')
+
+    cus=i.get('customize',{})
+
+    hosd=i['host_os_dict']
+    tosd=i['target_os_dict']
+
+    # Check platform
+    hplat=hosd.get('ck_name','')
+
+    ttags=tosd.get('tags',[])
+
+    win=tosd.get('windows_base','')
+    mingw=tosd.get('mingw','')
+
+    hproc=hosd.get('processor','')
+    tproc=tosd.get('processor','')
+
+    remote=tosd.get('remote','')
+    tbits=tosd.get('bits','')
+
+    env=i['env']
+
+    ep=cus['env_prefix']
+
+    fp=cus.get('full_path','')
+
+    pb=os.path.dirname(fp)
+    pinst=os.path.dirname(pb)
+
+    env[ep]=pinst
+
+    cus['path_bin']=pb
+
+    return {'return':0, 'bat':s}
