4601acebaa5f1974de84a88281377138632c1261
Vella Flavio <fvella@lxd-52348.unibz.it>
2018-10-08 13:24:55 +0000

    Add Generic BC on Multi-GPU

---
+++ package/lib-mgbc/scripts.linux/install.sh
@@ -0,0 +1,48 @@
+#! /bin/bash
+
+#
+# Installation script
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# - Flavio Vella, 2018.
+#
+
+# PACKAGE_DIR
+# INSTALL_DIR
+
+echo "**************************************************************"
+echo "Preparing vars for BC ..."
+
+
+
+CK_OPENMP="-fopenmp"
+if [ "${CK_HAS_OPENMP}" = "0"  ]; then
+  CK_OPENMP=""
+fi
+
+DEST_DIR=${INSTALL_DIR}/install/bin/
+
+if [ ! -d ${DEST_DIR} ] ; then
+      mkdir -p ${DEST_DIR}
+  fi
+
+cd ${INSTALL_DIR}/src/src
+      make \
+           DEST_DIR=${DEST_DIR}  \
+           GRAPH_GEN_LIB=$CK_ENV_LIB_GRAPH_GENERATOR_LIB \
+           MPI_HOME=${CK_ENV_LIB_MPI} \
+           CUDA_HOME=${CUDA_HOME} \
+           CUB_INCLUDE=${CK_ENV_LIB_CUB_INCLUDE} \
+           CUDA_ARCH=${CUDA_ARC} 
+
+
+if [ "${?}" != "0" ] ; then
+  echo "Error: make failed!"
+  exit 1
+fi
+
+
+return 0
---
+++ package/lib-mgbc/scripts.linux/post-install2.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+#
+# Installation script
+#
+# See CK LICENSE for licensing details.
+# See CK COPYRIGHT for copyright details.
+#
+# Developer(s):
+# Flavio Vella (Dividiti 2018)
+# PACKAGE_DIR
+# INSTALL_DIR
+
+ABC_LIB1=${INSTALL_DIR}/install/bin
+
+if [ ! -d "${ABC_LIB1}" ] && [ -d "${ABC_LIB1}" ] ; then
+  echo ""
+  echo "Do something "
+  echo ""
+
+  
+fi
+
+
+return 0
