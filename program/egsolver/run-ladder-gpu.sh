c6ba38f144fd29cf28cb7535ba37535c85f5a379
Flavio Vella <flavio@dividiti.com>
2018-11-29 15:12:09 +0000

    add script ladder gpu

---
+++ program/egsolver/run-ladder-gpu.sh
@@ -0,0 +1,10 @@
+DATASETPATH=/home/flavio/egsolver-dataset/ladder-dataset
+
+ck run program:egsolver --env.CK_EG_DATASET_PATH=${DATASETPATH} --cmd_key=run-egsolver-experiment-gpu
+ck run program:egsolver --env.CK_EG_DATASET_PATH=${DATASETPATH} --cmd_key=run-egsolver-experiment-gpu --env.CK_EG_SOLVER=egsolver_shfl_full_2tpv
+ck run program:egsolver --env.CK_EG_DATASET_PATH=${DATASETPATH} --cmd_key=run-egsolver-experiment-gpu --env.CK_EG_SOLVER=egsolver_shfl_full_4tpv
+ck run program:egsolver --env.CK_EG_DATASET_PATH=${DATASETPATH} --cmd_key=run-egsolver-experiment-gpu --env.CK_EG_SOLVER=egsolver_shfl_full_8tpv
+ck run program:egsolver --env.CK_EG_DATASET_PATH=${DATASETPATH} --cmd_key=run-egsolver-experiment-gpu --env.CK_EG_SOLVER=egsolver_shfl_full_16tpv
+ck run program:egsolver --env.CK_EG_DATASET_PATH=${DATASETPATH} --cmd_key=run-egsolver-experiment-gpu --env.CK_EG_SOLVER=egsolver_shfl_full_32tpv
+
+
