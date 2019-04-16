6c8cfbf8e806a5bc09dd85bdc45f97c3bf0b751e
fvella <flavio@dividiti.com>
2019-04-16 17:05:04 +0200

    fix script

--- program/egsolver/run-dataset-bucket-gpu.sh
+++ program/egsolver/run-dataset-bucket-gpu.sh
@@ -8,34 +8,19 @@ model=456081592a04c4a9
 # --deps.dataset-egsolver
 dataset=$clique
 echo "Clique" $dataset
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=5
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=10
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=15
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=20
+ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_OUTDEGREE=4 --env.CK_EG_LW=2 --env.CK_EG_UP=8
 
 dataset=$equiv
 echo "Equivalent" $dataset
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=5
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=10
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=15
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=20
-
-
+ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_OUTDEGREE=4 --env.CK_EG_LW=2 --env.CK_EG_UP=8
 
 dataset=$ladder
 echo "Ladder" $dataset
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=5
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=10
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=15
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=20
+ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_OUTDEGREE=4 --env.CK_EG_LW=2 --env.CK_EG_UP=8
 
 dataset=$model
 echo "Model" $dataset
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=5
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=10
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=15
-ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_THRESHOLD=20
-
+ck run program:egsolver  --cmd_key=run-egsolver-experiment-bucket-gpu  --deps.dataset-egsolver=$dataset --env.CK_EG_OUTDEGREE=4 --env.CK_EG_LW=2 --env.CK_EG_UP=8
 
 
 
